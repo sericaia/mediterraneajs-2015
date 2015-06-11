692aea2776014c233484bd11c635d9b13c0f58fc
Daniela Borges <alunassertiva@gmail.com>
2015-06-11 23:49:59 +0100

    validate params

--- 04-understanding-lifecycle/server/authApi.js
+++ 04-understanding-lifecycle/server/authApi.js
@@ -1,3 +1,5 @@
+var Joi = require('joi');
+
 var register = function (plugin, options, next) {
 
     plugin.route({
@@ -5,6 +7,28 @@ var register = function (plugin, options, next) {
         path:'/',
         handler: function (request, reply) {
             reply('super awesome index page!');
+        }
+    });
+
+    plugin.route({
+        method: 'GET',
+        path:'/welcome/{name}',  // ex. http://localhost:8000/welcome/daniela?mood=happy
+        handler: function (request, reply) {
+
+            var welcomeMsg = 'Welcome ' + request.params.name + "!\n";
+            welcomeMsg += 'Are you ' + request.query.mood + "?";
+
+            reply(welcomeMsg);
+        },
+        config: {
+            validate: {
+                params: {
+                    name: Joi.string().required()
+                },
+                query: {
+                    mood: Joi.string().valid(["happy","sad"]).default("happy")
+                }
+            }
         }
     });
 
